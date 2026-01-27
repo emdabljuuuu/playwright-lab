7cf39580490f7240ce55d7aa22900964a2301290
Marcin Wiewióra <marcinwiewiora@MacBook-Air-Marcin.local>
2026-01-27 15:56:25 +0100

    gitlab

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,9 +28,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-  // process.env.CI ? ['blob'] : ['html'],
-  [ 'html'], // Stary raport (na wszelki wypadek)
-  [ 'allure-playwright', { outputFolder: 'allure-results' }] // Nowy raport
+    ['html'], // Twój stary raport HTML
+    // Nowy raport XML dla GitLaba:
+    ['junit', { outputFile: 'test-results/results.xml' }] 
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
