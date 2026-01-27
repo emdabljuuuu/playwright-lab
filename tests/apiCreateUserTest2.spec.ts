03afe6064bbd09ec2f782f13f315e2fe691e644f
Marcin Wiewióra <marcinwiewiora@MacBook-Air-Marcin.local>
2026-01-27 16:00:32 +0100

    gitlab

---
+++ tests/apiCreateUserTest2.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+import { CreateUserDto, UserApiClient, UserDto } from '../api/UserApiClient';
+import { UserFactory } from '../factories/UserFactory';
+
+const user: CreateUserDto = UserFactory.createDefault();
+
+test('Create user test 2', async ({request}) => {
+  const userApiClient = new UserApiClient(request);
+  const userResponse: UserDto = await userApiClient.createUser(user);
+  expect(userResponse.id).toBe(11);
+});
