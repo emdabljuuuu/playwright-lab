82fba649cd5d9357054e17fd3a00cda775d63d39
Marcin Wiewióra <marcinwiewiora@MacBook-Air-Marcin.local>
2026-01-30 17:58:45 +0100

    gitlab

--- playwright.config.ts
+++ playwright.config.ts
@@ -63,10 +63,10 @@ export default defineConfig({
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
     // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: 'mobile-safari',
+      use: { ...devices['iPhone 12'] },
+    },
 
     /* Test against branded browsers. */
     // {
